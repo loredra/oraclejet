d07a5fef948b81d15d6dfc4feeef40140cb9d619
dgmo <darius.motosan@pst.ag>
2016-07-19 13:31:15 +0200

    FixCurrentGitProblem

---
+++ js/lang/lang.en.js
@@ -0,0 +1,56 @@
+english = {
+    searchPage: {
+        basic: {
+            search: "search",
+            country: "Country",
+            list: "List"
+        },
+        advancedSearch: {
+            title: "Advanced Search",
+            defaultButton: "Default",
+            wordPercentage: {
+                text: "Word Percentage",
+                definition: "The minimum percentage value of an word to accept a match"
+            },
+            phrasePercentage: {
+                text: "Phrase Percentage"
+            },
+            scoreAlgorithm: {
+                text: "Score Algorithm"
+            },
+            wordsDistanceAlgorithm: {
+                text: "Words Distance Algorithm"
+            }
+        },
+        hits: "results",
+        searchedEntityProperty: {
+            percentage: "Percentage",
+            country: "Country",
+            addressStatus: "without address",
+            countryStatus: "worldwide",
+            found: "Found",
+            entities: "Entities"
+        }
+    },
+    detailsPage:{
+        name: "Name",
+        primaryAddress: "Primary Address",
+        identification: "Identification",
+        country: "Country",
+        sanctionListsDescription1:"Mentioned in",
+        sanctionListsDescription2:"Sanction Lists",
+        knownNamesDescription1:"Have",
+        knownNamesDescription2:"known names",
+        knownAddressDescription1:"Have",
+        knownAddressDescription2:"known addresses",
+        hideNodeName:"Hide node's name",
+        hideLinkName:"Hide link's name",
+        tooltipType:"Type",
+        tooltipStatus:"Status",
+        tooltipListDetails1:"Listed in",
+        tooltipListDetails2:"Sanction Lists",
+        knownNames:"Known Names",
+        knownAddresses:"Known Addresses",
+        generalInfo:"General Information"
+    }
+};
---
+++ js/lang/lang.ge.js
@@ -0,0 +1,56 @@
+german = {
+    searchPage: {
+        basic: {
+            search: "suche",
+            country: "Land",
+            list: "Liste"
+        },
+        advancedSearch: {
+            title: "Erweiterte Suche",
+            defaultButton: "Standard",
+            wordPercentage: {
+                text: "Wortes Prozentsatz",
+                definition: "Der Mindestprozentwert des auf Wort eine Übereinstimmung zu akzeptieren"
+            },
+            phrasePercentage: {
+                text: "Phrase Prozentsatz"
+            },
+            scoreAlgorithm: {
+                text: "Score Algorithmus"
+            },
+            wordsDistanceAlgorithm: {
+                text: "Wörter Entfernung Algorithmus"
+            }
+        },
+        hits: "werte",
+        searchedEntityProperty: {
+            percentage: "Prozentsatz",
+            country: "Land",
+            addressStatus: "ohne adresse",
+            countryStatus: "weltweit",
+            found: "Gefunden",
+            entities: "Entitäten"
+        }
+    },
+    detailsPage:{
+        name: "Name",
+        primaryAddress: "Hauptadresse",
+        identification: "Identifizierung",
+        country: "Land",
+        sanctionListsDescription1:"Erwähnt in",
+        sanctionListsDescription2:"Sanktionslisten",
+        knownNamesDescription1:"Haben",
+        knownNamesDescription2:"bekannte Namen",
+        knownAddressDescription1:"Haben",
+        knownAddressDescription2:"bekannte Adressen",
+        hideNodeName:"Ausblenden Knoten Name",
+        hideLinkName:"Ausblenden Link Name",
+        tooltipType:"Art",
+        tooltipStatus:"Status",
+        tooltipListDetails1:"Aufgelistet in",
+        tooltipListDetails2:"Sanktionsliste",
+        knownNames:"Bekannte Namen",
+        knownAddresses:"Bekannte Addressen",
+        generalInfo:"Allgemeine Information"
+    }
+};
